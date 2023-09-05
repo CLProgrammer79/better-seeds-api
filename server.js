fd1095201c50880b56eaaefa911a09759cba5abf
Claudio Leo <claudiolbp@gmail.com>
2023-09-05 02:28:56 -0700

    functionality 90% style 90%

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const videosRouter = require("./routes/videosRouter");
 const port = process.env.PORT || 5050;
 
 app.use(cors());
-app.use(express.static("public"));
 app.use(express.json());
+app.use(express.static("public"));
 // app.use(express.static("better-seeds-api/public"));
 
 app.use("/video-library", videosRouter);
@@ -15,6 +15,7 @@ app.use("/video-library", videosRouter);
 // Start the server
 const PORT = process.env.PORT || 5050;
 app.use((req, res) => {
+  console.log("Server Running ");
   res
     .status(404)
     .sendFile(path.join(__dirname, "public", "errors", "404.html"));
